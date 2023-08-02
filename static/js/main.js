f10bba49ecbe3a5b418c33e3b2317ad840e53825
syu26516 <JAMESYU1209@GMAIL.COM>
2023-08-02 15:02:17 -0700

    Updated with some improvements
    1. Added tour feature.
    2. Updated readme.html with latest fonts and styles using a new example.
    3. Added level difficulty reminder to remind the assistant regarding with difficulty level.
    4. Re-enabled API Model and API Key input.
    5. Added UID to exported log files.

--- static/js/main.js
+++ static/js/main.js
@@ -18,6 +18,9 @@ let easySuffix = "Please provide short and simple replies, keeping each reply to
 let moderateSuffix = "Please provide short replies, keeping each reply to only one sentence in intermediate level vocabulary and grammar.";   
 let expertSuffix = "Please provice concise and creative replies, keeping each reply to no more than three sentences in high-level vocabulary and grammar."; 
 
+// Counter to remind the left assistant to stick with the current difficulty level:
+let leftCounter = 3; // remind it per three rounds of conversation
+
 // Record user role, environment, and assistant role:
 let userRole = "";
 let aiRole = "";
@@ -42,10 +45,7 @@ function setupConfig() {
    chatLevel = $("#chat-level").val();
    userSystemInput = $("#system-context").val();
    userAPIKey = $("#user-api-key").val();
-
-   // change this back for future deploying:
-   // userModel = $("#chat-model").val();
-   userModel = "gpt-3.5-turbo";  // for testing only
+   userModel = $("#chat-model").val();
 
    // Show chat windows:
    if (!configClicked && userRole.replace(/\s/g, "") != "" 
@@ -66,7 +66,7 @@ function setupConfig() {
       leftSystemConfig = "You are the " + aiRole + " at this " + chatContext + ", " +
          "and the user is the " + userRole + " at this " + chatContext + ". " +
          "You and the user will have a conversation in " + chatLanguage + ". " +
-         "Please always keep in mind your and the user's roles in the above scenario";
+         "Please make sure your replies are culturally appropriate, and always keep in mind your and the user's roles in the above scenario.";
       if (userSystemInput.replace(/\s/g, "") != "") {
          leftSystemConfig = leftSystemConfig + " " + userSystemInput;
       }
@@ -185,12 +185,12 @@ function sendMessage(chatBoxNumber) {
    // Check to see if on right window and message is a instruction message of "-help", "-level" or "system":
    if (chatBoxNumber == 2 && userInput.startsWith("/")) {
       // This should be considered as a system message, we append it in right export log:
-      rightExportLog.push(getCurrentTime() + "User: " + userInput);
+      rightExportLog.push(getCurrentTime() + "USER: " + userInput);
 
       if (userInput.startsWith("/help")) {
          addSystemMessage("/level [level]:<br>&emsp;&emsp;&emsp;Change current level to [level]<br>&emsp;&emsp;&emsp;[level] must be Easy, Moderate, or Expert", 1);
          addSystemMessage("/system [message]:<br>&emsp;&emsp;&emsp;Add instruction to chat bot<br>&emsp;&emsp;&emsp;e.g. You are upset./Start sentence with...", 1);
-         rightExportLog.push(getCurrentTime() + "System: /level [level]: Change current level to [level], [level] must be Easy, Moderate, or Expert.\t" +
+         rightExportLog.push(getCurrentTime() + "SYSTEM: /level [level]: Change current level to [level], [level] must be Easy, Moderate, or Expert.\t" +
             "/system [message]: Add instruction to chat bot. e.g. You are upset./Start sentence with...");
       } else if (userInput.startsWith("/level")) {
          // Get level:
@@ -200,15 +200,17 @@ function sendMessage(chatBoxNumber) {
          if (newLevel === "Easy" || newLevel === "Moderate" || newLevel === "Expert") {
             if (newLevel === chatLevel) {
                addSystemMessage("---Level unchanged---", 2);
-               rightExportLog.push(getCurrentTime() + "System: ---Level unchanged---");
+               rightExportLog.push(getCurrentTime() + "SYSTEM: ---Level unchanged---");
             } else {
+               // Update left reminder counter:
+               leftCounter = 3;
                // Update select value to avoid confusion:
                $("#chat-level").prop("disabled", false);
                $("#chat-level").val(newLevel);
                $("#chat-level").prop("disabled", true);
                // Show message:
                addSystemMessage("---Level changed from " + chatLevel + " to " + newLevel + "---", 2);
-               rightExportLog.push(getCurrentTime() + "System: ---Level changed from " + chatLevel + " to " + newLevel + "---");
+               rightExportLog.push(getCurrentTime() + "SYSTEM: ---Level changed from " + chatLevel + " to " + newLevel + "---");
                // Update chat level:
                chatLevel = newLevel;
                // Add new system message to left log history to take effect:
@@ -227,7 +229,7 @@ function sendMessage(chatBoxNumber) {
             }
          } else {
             addSystemMessage("---Invalid level---", 2);
-            rightExportLog.push(getCurrentTime() + "System: ---Invalid level---");
+            rightExportLog.push(getCurrentTime() + "SYSTEM: ---Invalid level---");
          }
       } else if (userInput.startsWith("/system")) {
          // Get system message:
@@ -235,10 +237,10 @@ function sendMessage(chatBoxNumber) {
          // Add new system message to left log history:
          updateLog(1, {'role': 'system', 'content': newSystemMessage});
          addSystemMessage("System message: \"" + newSystemMessage + "\" has been successfully configured.", 1);
-         rightExportLog.push(getCurrentTime() + "System: " + "System message: \"" + newSystemMessage + "\" has been successfully configured.");
+         rightExportLog.push(getCurrentTime() + "SYSTEM: " + "System message: \"" + newSystemMessage + "\" has been successfully configured.");
       } else {
          addSystemMessage("---Invalid instruction---<br>---Type /help for more information---", 2);
-         rightExportLog.push(getCurrentTime() + "System: ---Invalid instruction---\t---Type /help for more information---");
+         rightExportLog.push(getCurrentTime() + "SYSTEM: ---Invalid instruction---\t---Type /help for more information---");
       }
    } else {
       // add user message to log history for export:
@@ -291,6 +293,13 @@ function sendMessage(chatBoxNumber) {
             } else {
                const currUserMessage = getCurrentTime() + "Teacher: " + assistantReply;
                rightExportLog.push(currUserMessage);
+            }
+
+            // Update left counter for difficulty reminder:
+            leftCounter--;
+            if (leftCounter === 0) {
+               leftCounter = 3;
+               updateLog(1, {'role': 'system', 'content': levelSuffix});
             }
          },
          error: function (error) {
@@ -353,6 +362,25 @@ function getCurrentTime() {
    return formattedDateTime;
 }
 
+// Generate UID for log file based on current time:
+function getUID() {
+   const currentTime = new Date();
+
+   // current date components:
+   const year = currentTime.getFullYear();
+   const month = currentTime.getMonth() + 1; // months are zero-based, add 1 to get the actual month
+   const day = currentTime.getDate();
+
+   // current time components:
+   const hours = currentTime.getHours();
+   const minutes = currentTime.getMinutes();
+   const seconds = currentTime.getSeconds();
+
+   // Combine the extracted parts to form the UID
+   const uid = `${year}${month}${day}${hours}${minutes}${seconds}`;
+   return uid;
+}
+
 // Generate a txt file for export log history and auto-downloading:
 function generateLogFile(chatBoxNumber) {
    // one line per chat history
@@ -369,9 +397,9 @@ function generateLogFile(chatBoxNumber) {
    link.href = url;
 
    if (chatBoxNumber == 1) {
-      link.download = userRole + "_" + aiRole + "_chat_log.txt";
+      link.download = getUID() + "_" + userRole + "_" + aiRole + "_log.txt";
    } else {
-      link.download = "student_teacher_chat_log.txt";
+      link.download = getUID() + "_" + "Student_Teacher_log.txt";
    }
 
    // trigger the download:
@@ -379,4 +407,77 @@ function generateLogFile(chatBoxNumber) {
 
    // after the download clean up the URL object:
    URL.revokeObjectURL(url);
+}
+
+// Tour function.
+function startTour() {
+   introJs().setOptions({
+      keyboardNavigation: false,
+      steps: [
+         {
+            intro: "Welcome!<br><br>This is a language learning tool that allows you to role-play different characters in a target language of your choosing.<br><br>Let's get started!"
+         }, 
+         {
+            element: document.querySelector('#info-form'),
+            intro: "This is the configuration section. Here, you can set information about the role-playing scenario."
+         },
+         {
+            element: document.querySelector('#chat-context'),
+            intro: "Pick the role-play scenario, e.g., Restaurant."
+         },
+         {
+            element: document.querySelector('#ai-role'),
+            intro: "Pick chatbot's role, e.g., Waiter."
+         },
+         {
+            element: document.querySelector('#user-role'),
+            intro: "Pick your role, e.g., Customer."
+         },{
+            element: document.querySelector('#chat-language'),
+            intro: "Pick the language you want to learn. <br><br>Please refer to: <a class='custom-link' href='https://acutrans.com/languages-supported-by-chatgpt/' target='_blank'>supported languages</a>."
+         },
+         {
+            element: document.querySelector('#chat-level'),
+            intro: "Easy level: replies from the chatbot are usually short and simple.<br><br>Moderate level: replies are short but with intermediate level grammars and vocabularies." +
+               "<br><br>Expert level: replies are more lengthy and creative.",
+         },
+         {
+            element: document.querySelector('#chat-model'),
+            intro: "Choose the chat model.<br><br>Note: some API keys do not support GPT-4."
+         },
+         {
+            element: document.querySelector('#system-context'),
+            intro: "You can give the chatbot an optional command to behave in a certain way, e.g. Today is a rainy day."
+         },
+         {
+            element: document.querySelector('#user-api-key'),
+            intro: "You can enter your API key for one time use.<br><br>We do NOT store or cache your API keys."
+         },
+         {
+            element: document.querySelector('#start-button'),
+            intro: "Click here begin! <br><br>You will see two chat windows pop up below."
+         },
+         {
+            element: document.querySelector('#inputBox1'),
+            intro: "You can start chat in the language and the role-playing scenario you've just configured.",
+         },
+         {
+            element: document.querySelector('#chatBox1'),
+            intro: "The chatbot will chat with you based on your inputs.",
+         },
+         {
+            element: document.querySelector('#inputBox2'),
+            intro: "You can ask questions regarding with the chatbot's replies in any language here.<br><br>Please make sure to indicate the related language in your questions, e.g. How to say xxx in French..." +
+               "<br><br>You can also change language difficulty level and add additional notes for the chatbot. Type /help to find out more."
+         },
+         {
+            element: document.querySelector('#export-button-1'),
+            intro: "You can download your chat history anytime."
+         },
+         {
+            element: document.querySelector('#restart-button'),
+            intro: "When you're done with the current chat, click here to restart everything!"
+         }
+      ]
+   }).start();
 }
