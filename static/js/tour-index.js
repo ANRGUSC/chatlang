1cf4feb813e962d8d1bd3f0877f8bf4e005fddb4
Jared Coleman <jaredraycoleman@gmail.com>
2023-09-24 13:30:34 -0700

    Fixes tour

--- static/js/tour-index.js
+++ static/js/tour-index.js
@@ -40,46 +40,9 @@ function startTour() {
              intro: "You can give the chatbot an optional command to behave in a certain way, e.g. Today is a rainy day."
           },
           {
-             element: document.querySelector('#api_key'),
-             intro: "You can enter your API key for one time use.<br><br>We do NOT store or cache your API keys."
-          },
-          {
-             element: document.querySelector('#tutor_language'),
-             intro: "You can choose the language of the tutor (your native language)."
-          },
-          {
              element: document.querySelector('#btn-new-conversation'),
              intro: "Click here begin! <br><br>You will see two chat windows pop up below."
           },
-    //       {
-    //          element: document.querySelector('#inputBox1'),
-    //          intro: "You can start chat in the language and the role-playing scenario you've just configured.",
-    //       },
-    //       {
-    //          element: document.querySelector('#chatBox1'),
-    //          intro: "The chatbot will chat with you based on your inputs.",
-    //       },
-    //       {
-    //          element: document.querySelector('#inputBox2'),
-    //          intro: "You can ask questions regarding with the chatbot's replies in any language here.<br><br>Please make sure to indicate the related language in your questions, e.g. How to say xxx in French..." +
-    //             "<br><br>You can also change language difficulty level and add additional notes for the chatbot. Type /help to find out more."
-    //       },
-    //       {
-    //          element: document.querySelector('#chatBox2'),
-    //          intro: "The chatbot will answer your questions like a language teacher. <br><br>All system messages will show in a different color.",
-    //       },
-    //       {
-    //          element: document.querySelector('#export-button-1'),
-    //          intro: "You can download your chat history anytime."
-    //       },
-    //       {
-    //          element: document.querySelector('#refresh-button'),
-    //          intro: "You can clear your current chat history and restart it with the same Settings."
-    //       },
-    //       {
-    //          element: document.querySelector('#restart-button'),
-    //          intro: "Wanna try something new? <br><br>You can reset everything!"
-    //       }
        ]
     }).start();
  }
